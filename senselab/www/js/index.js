ceee6f126534d0a42da1ec8979b428926c9db2a8
Pablo Ferrer <100330656@alumnos.uc3m.es>
2018-12-11 15:51:36 +0100

    Avances 2018-12-11

--- senselab/www/js/index.js
+++ senselab/www/js/index.js
@@ -17,11 +17,22 @@
 * under the License.
 */
 
+var socket;
+
 $('#TriggerTypeSelector').change(function ()
 {
 	var selector = '#trigger-' + $(this).val();
 
-	$('.myCollapse').collapse('hide');
+	$('.myCollapseTriggers').collapse('hide');
+
+	$(selector).collapse('show');
+});
+
+$('#ActionTypeSelector').change(function ()
+{
+	var selector = '#action-' + $(this).val();
+
+	$('.myCollapseActions').collapse('hide');
 
 	$(selector).collapse('show');
 });
@@ -31,56 +42,157 @@ $('#SliderFrom').on('input', function ()
 	$('#SliderTo').html($('#SliderFrom').val() + '%')
 });
 
+function mySubmit (strAction)
+{
+	var unindexed_array = $('#NewRuleForm').serializeArray();
+	var values = {};
+
+	$.map(unindexed_array, function (n, i)
+	{
+		values[n['name']] = n['value'];
+	});
+	console.log(values);
+	ruleData = {
+		'rule_name': undefined,
+		'trigger_type_id': undefined,
+		'trigger_data_int': undefined,
+		'trigger_data_time': undefined,
+		'action_type_id': undefined,
+		'action_data_text': undefined
+	};
+
+	ruleData.rule_name = values['rule_name'];
+	ruleData.trigger_type_id = values['trigger_type'];
+	switch (ruleData.trigger_type_id)
+	{
+		case '3':
+		case '4':
+			ruleData.trigger_data_int = values['trigger_data_temp'];
+			break;
+		case '7':
+		case '8':
+			ruleData.trigger_data_int = values['trigger_data_light'];
+			break;
+		case '11':
+		case '12':
+			ruleData.trigger_data_int = values['trigger_data_strength'];
+			break;
+		case '13':
+		case '14':
+			ruleData.trigger_data_int = values['trigger_data_humidity'];
+			break;
+		default:
+			ruleData.trigger_data_int = undefined;
+			break;
+	}
+	switch (ruleData.trigger_type_id)
+	{
+		case '9':
+		case '10':
+			ruleData.trigger_data_time = values['trigger_data_time'];
+			break;
+
+		default:
+			break;
+	}
+	if (strAction == '234')
+	{
+		ruleData.action_type_id = values['action_type'];
+	} else
+	{
+		ruleData.action_type_id = strAction;
+	}
+	switch (ruleData.action_type_id)
+	{
+		case '1':
+			ruleData.action_data_text = values['action_data_text'];
+			break;
+		default:
+			ruleData.action_data_text = undefined;
+			break;
+	}
+
+	console.log(ruleData);
+
+	socket.emit('new_rule', JSON.stringify(ruleData));
+
+	return false;
+}
+
+function getRuleList ()
+{
+	socket.emit('get_rule_list');
+	return false;
+}
+
+function createCard (cardData)
+{
+	var trigger_desc_full;
+	switch (cardData.trigger_type_id)
+	{
+		case 3:
+		case 4:
+			trigger_desc_full = cardData.trigger_description_long + ' ' + cardData.trigger_data_int + '°C';
+			break;
+		case 7:
+		case 8:
+			trigger_desc_full = cardData.trigger_description_long + ' ' + cardData.trigger_data_int + '%';
+			break;
+		case 9:
+		case 10:
+			trigger_desc_full = cardData.trigger_description_long + ' ' + cardData.trigger_data_time;
+			break;
+		default:
+			trigger_desc_full = cardData.trigger_description_long;
+			break;
+	}
+	var action_desc_full;
+	switch (cardData.action_type_id)
+	{
+		case 1:
+		case 4:
+			action_desc_full = cardData.action_description_long + ' ' + cardData.action_data_text;
+			break;
+		default:
+			action_desc_full = cardData.action_description_long;
+			break;
+	}
+	var cardTemplate = [
+		'<div class="card mb-3">',
+		'<div class="card-body">',
+		'<h3 class="card-title">',
+		cardData.rule_name,
+		'</h3>',
+		'<p class="card-title">',
+		trigger_desc_full,
+		'</p>',
+		'<p class="card-title">',
+		action_desc_full,
+		'</p>',
+		'</div>',
+		'</div>'
+	];
+	return $(cardTemplate.join(''));
+}
+
+
 $(document).ready(function ()
 {
-	var socket = io.connect('http://localhost:8080');
-	$('#NewRuleForm').submit(function ()
+	socket = io.connect('http://localhost:8080');
+
+	socket.on('rule_list', function (data)
 	{
-		var unindexed_array = $('#NewRuleForm').serializeArray();
-		var values = {};
-		$.map(unindexed_array, function (n, i)
+		var rule_list_parsed = JSON.parse(data);
+
+		console.log(rule_list_parsed);
+
+		var cards = $();
+		$.each(rule_list_parsed, function (index, value)
 		{
-			values[n['name']] = n['value'];
+			console.log(index + ' - ' + value.rule_name);
+			cards = cards.add(createCard(value))
 		});
-		ruleData = {
-			'trigger_type_id': undefined,
-			'trigger_data_int': undefined,
-			'trigger_data_datetime': undefined,
-			'action_type_id': undefined,
-			'action_data_text': undefined
-		};
-		ruleData.trigger_type_id = values['trigger_type'];
-		switch (ruleData.trigger_type_id)
-		{
-			case '3':
-			case '4':
-				ruleData.trigger_data_int = values['trigger_data_temp'];
-				break;
-			case '7':
-			case '8':
-				ruleData.trigger_data_int = values['trigger_data_light'];
-				break;
-			case '11':
-			case '12':
-				ruleData.trigger_data_int = values['trigger_data_strength'];
-				break;
-			case '13':
-			case '14':
-				ruleData.trigger_data_int = values['trigger_data_humidity'];
-				break;
-			default:
-				ruleData.trigger_data_int = undefined;
-				break;
-		}
-		ruleData.trigger_data_datetime = values['trigger_data_datetime'];
-		ruleData.action_type_id = values['action_type_id'];
-		ruleData.action_data_text = values['action_data_text'];
-
-		console.log(ruleData);
-
-		socket.emit('new_rule', JSON.stringify(ruleData));
-
-		return false;
+		$('#CardArea').empty();
+		$('#CardArea').append(cards);
 	});
 });
-
--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const mysql = require('mysql');
 const port = 8080;
+
 var db_config = {
 	host: 'ni2141493-2.web13.nitrado.hosting',
 	port: 3306,
@@ -12,10 +13,12 @@ var db_config = {
 };
 var connection;
 
+var sqlInsertRule = 'INSERT INTO ni2141493_2sql1.rule SET ?';
+var sqlSelectRule = 'SELECT * FROM rule INNER JOIN trigger_type ON rule.trigger_type_id = trigger_type.trigger_type_id INNER JOIN action_type ON rule.action_type_id = action_type.action_type_id';
+
 function handleDisconnect ()
 {
 	connection = mysql.createConnection(db_config);
-	console.log('Connected to database with thread id: ' + connection.threadId)
 	connection.connect(function (err)
 	{
 		if (err)
@@ -24,6 +27,7 @@ function handleDisconnect ()
 			console.error('Attempting to reconnect...');
 			setTimeout(handleDisconnect, 2000);
 		}
+		console.log('Connected to database with thread id: ' + connection.threadId)
 	});
 	connection.on('error', function (err)
 	{
@@ -48,9 +52,36 @@ io.on('connection', function (socket)
 	socket.on('new_rule', function (data)
 	{
 		console.log('\n\nMessage received: new_rule');
-		new_rule_parsed = JSON.parse(data);
+		var new_rule_parsed = JSON.parse(data);
 
 		console.log(new_rule_parsed);
+		var values = [
+			// new_rule_parsed.rule_name,
+			// new_rule_parsed.trigger_type_id,
+			// new_rule_parsed.trigger_data_int,
+			// new_rule_parsed.trigger_data_time,
+			// new_rule_parsed.action_type_id,
+			// new_rule_parsed.action_data_text
+			'aaa', 1, null, null, 2, null
+		]
+		var query = connection.query(sqlInsertRule, new_rule_parsed, function (err, result)
+		{
+			if (err)
+			{
+				console.log(query.sql);
+				throw err;
+			}
+			console.log('Number of records inserted: ' + result.affectedRows);
+		});
+	});
 
+	socket.on('get_rule_list', function ()
+	{
+		var query = connection.query(sqlSelectRule, function (err, result)
+		{
+			if (err) throw err;
+			console.log(result)
+			socket.emit('rule_list', JSON.stringify(result));
+		});
 	});
 });
